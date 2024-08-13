1fc9b7537518faa5b7f997a4342c2e2ea4c562a0
Guilherme <>
2024-08-13 10:11:14 -0300

    Refatoração de código em MathController

---
+++ 06_ExceptionHandler/src/main/java/br/com/guisamp/rest_with_spring_boot_and_java_erudio/MathController.java
@@ -0,0 +1,78 @@
+package br.com.guisamp.rest_with_spring_boot_and_java_erudio;
+
+import org.springframework.web.bind.annotation.RestController;
+import br.com.guisamp.rest_with_spring_boot_and_java_erudio.exceptions.UnsuportedMathOperationExceptione;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/calculator")
+public class MathController {
+    
+    @GetMapping("/basicoperations/{strNumberOne}/{operator}/{strNumberTwo}")   
+    public Double operacoesBasicas(@PathVariable String strNumberOne, @PathVariable String operator, @PathVariable String strNumberTwo){
+        if(!isNumeric(strNumberOne) || (!isNumeric(strNumberTwo))){
+            throw new UnsuportedMathOperationExceptione("Parametro tem que ser do tipo númerico");
+        }
+
+        Double numberOne = convertToDouble(strNumberOne);
+        Double numberTwo = convertToDouble(strNumberTwo);
+        switch (operator) {
+            case "sum":
+                return numberOne + numberTwo;
+            
+            case "sub":
+                return numberOne - numberTwo;
+            
+            case "mul":
+                return numberOne * numberTwo;
+            
+            case "div":
+                if(numberTwo == 0){
+                    throw new UnsuportedMathOperationExceptione("Divisor não pode ser igual a zero");
+                }
+                return numberOne / numberTwo;
+                          
+            default:
+                throw new UnsuportedMathOperationExceptione("Digite um operador válido");
+        }
+    }
+
+    @GetMapping("/squareroot/{number}")
+    public Double raizQuadrada(@PathVariable String number) {
+        if (!isNumeric(number)) {
+            throw new UnsuportedMathOperationExceptione("Parâmetro tem que ser do tipo numérico");
+        }
+
+        double num = convertToDouble(number);
+        if (num < 0) {
+            throw new UnsuportedMathOperationExceptione("Não é possível calcular a raiz quadrada de um número negativo");
+        }
+
+        return Math.sqrt(num);
+    }
+
+    @GetMapping("/average/{strNumberOne}/{strNumberTwo}")
+    public Double getMethodName(@PathVariable String strNumberOne, @PathVariable String strNumberTwo) {
+        if(!isNumeric(strNumberOne) || (!isNumeric(strNumberTwo))){
+            throw new UnsuportedMathOperationExceptione("Parâmetro tem que ser do tipo numérico");
+        }
+
+        return (convertToDouble(strNumberOne) + convertToDouble(strNumberTwo))/2; 
+    }
+    
+
+    public static Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0d; 
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0d;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false; 
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
---
+++ 06_ExceptionHandler/src/main/java/br/com/guisamp/rest_with_spring_boot_and_java_erudio/exceptions/handler/GlobalErrorHandler.java
@@ -0,0 +1,29 @@
+package br.com.guisamp.rest_with_spring_boot_and_java_erudio.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import br.com.guisamp.rest_with_spring_boot_and_java_erudio.exceptions.ExceptionResponse;
+import br.com.guisamp.rest_with_spring_boot_and_java_erudio.exceptions.UnsuportedMathOperationExceptione;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+    @ExceptionHandler(UnsuportedMathOperationExceptione.class)
+    public ResponseEntity<ExceptionResponse> unsuportedMathOperationExceptione(UnsuportedMathOperationExceptione exception, WebRequest request){
+    	ExceptionResponse response = new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+    	
+    	return new ResponseEntity<ExceptionResponse>(response,HttpStatus.INTERNAL_SERVER_ERROR); 
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception exception){
+        String errorMessage = "Ocorreu um erro inesperado: " + exception.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  
+}
